85224a10ac337fd6f1983f63719d2804cfd889e7
Semen Popov <popovsemen666@gmail.com>
2021-12-12 17:21:06 +0300

    [AS-1] Add packages and create app context for db

---
+++ AkinaSpeedStars.DAL/Data/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using AkinaSpeedStars.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkinaSpeedStars.DAL.Data
+{
+    internal class AppContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Kit> Kits { get; set; }
+        public DbSet<ModelCode> ModelCodes { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<PartGroup> PartGroups { get; set; }
+        public DbSet<PartSubgroup> PartSubgroups { get; set; }
+        public DbSet<PartTree> PartTrees { get; set; }
+        public DbSet<Scheme> Schemes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\AkinaSpeedStars;Initial Catalog=AppDB;");
+        }
+    }
+}
