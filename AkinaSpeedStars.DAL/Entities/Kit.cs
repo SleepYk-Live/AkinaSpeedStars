e57749bfb3299d6582cbaef491a18e967c232317
Semen Popov <popovsemen666@gmail.com>
2021-12-12 16:43:18 +0300

    [AS-1] Add destination enum and correct some entities

--- AkinaSpeedStars.DAL/Entities/Kit.cs
+++ AkinaSpeedStars.DAL/Entities/Kit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AkinaSpeedStars.DAL.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,13 +12,13 @@ namespace AkinaSpeedStars.DAL.Entities
         public int Id { get; set; }
         public string Engine { get; set; }
         public string Body { get; set; }
-        public string Grade { get; set; } // make it enum
+        public string Grade { get; set; } 
         public bool IsATM { get; set; }
         public string GearShiftType { get; set; }
         public bool IsDriverPositionLeft { get; set; }
         public int NumberOfDoors { get; set; }
-        public string Destination { get; set; } // currency enum
-        public string AdditionalDestination { get; set; } // currency enum
+        public Destination Destination { get; set; } 
+        public Destination AdditionalDestination { get; set; }
         public IEnumerable<PartGroup> PartGroups { get; set; }
     }
 }
